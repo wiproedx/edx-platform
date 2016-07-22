ffc8f63bf1bfc2db240b9cce85f2a70fc07a2b64
Qubad786 <muhammadrehan69@gmail.com>
2016-07-22 17:02:36 +0500

    move fix to importer module, add more test cases

--- cms/djangoapps/contentstore/tests/test_libraries.py
+++ cms/djangoapps/contentstore/tests/test_libraries.py
@@ -63,7 +63,7 @@ class LibraryTestCase(ModuleStoreTestCase):
         self.assertIsInstance(lib_key, LibraryLocator)
         return lib_key
 
-    def _add_library_content_block(self, course, library_key, other_settings=None):
+    def _add_library_content_block(self, course, library_key, publish_item=False, other_settings=None):
         """
         Helper method to add a LibraryContent block to a course.
         The block will be configured to select content from the library
@@ -74,7 +74,7 @@ class LibraryTestCase(ModuleStoreTestCase):
             category='library_content',
             parent_location=course.location,
             user_id=self.user.id,
-            publish_item=False,
+            publish_item=publish_item,
             source_library_id=unicode(library_key),
             **(other_settings or {})
         )
@@ -158,7 +158,7 @@ class TestLibraries(LibraryTestCase):
         with modulestore().default_store(ModuleStoreEnum.Type.split):
             course = CourseFactory.create()
 
-        lc_block = self._add_library_content_block(course, self.lib_key, {'max_count': num_to_select})
+        lc_block = self._add_library_content_block(course, self.lib_key, other_settings={'max_count': num_to_select})
         self.assertEqual(len(lc_block.children), 0)
         lc_block = self._refresh_children(lc_block)
 
