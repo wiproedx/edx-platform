b58b8d4d866baf2715718135ed188c1ef3464782
satyarapelly <satya.rapelly@gmail.com>
2017-01-20 04:04:26 -0800

    Wrong namespaces

--- lms/djangoapps/grades/migrations/0004_visibleblocks_course_id.py
+++ lms/djangoapps/grades/migrations/0004_visibleblocks_course_id.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 from opaque_keys.edx.keys import CourseKey
-from openedx.core.djangoapps.xmodule_django.models import CourseKeyField
+from xmodule_django.models import CourseKeyField
 
 
 class Migration(migrations.Migration):
