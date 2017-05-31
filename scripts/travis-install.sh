ca9a4c62868e2367282579af5612e7be187b8131
Kabir Khan <kakh@microsoft.com>
2017-05-31 16:50:17 -0700

    Dockerfile update

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,4 +1,4 @@
-rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install 6.9.4
+rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && . ~/.nvm/nvm.sh && nvm install 6.9.4
 npm install
 pip install setuptools
 pip install --exists-action w -r requirements/edx/paver.txt
