412fc8982d7416c9ba4180a707e31d4e248b4e56
Kabir Khan <kakh@microsoft.com>
2017-06-01 14:15:33 -0700

    adding mongo shell to container

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -18,7 +18,11 @@ pip install --exists-action w -r requirements/edx/local.txt
 # Install a version which falls within that range.
 pip install --exists-action w pbr==0.9.0
 pip install --exists-action w -r requirements/edx/base.txt
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update -y
+sudo apt-get install -y mongodb-org
 sudo apt-get install libxmlsec1-dev -y
 sudo apt-get install swig -y
+
 if [ -e requirements/edx/post.txt ]; then pip install --exists-action w -r requirements/edx/post.txt ; fi
