a45e921c38d47565682a31173d1141c8b5e7ec44
Kabir Khan <kakh@microsoft.com>
2017-06-29 13:57:46 -0700

    fixed indentation typo

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
+
 docker exec -i devstack /bin/bash -s <<EOF
-  echo 'Fixing Mongo'
-  sudo -S rm /edx/var/mongo/mongodb/mongod.lock
-  sudo -S mongod -repair --config /etc/mongod.conf
-  sudo -S chown -R mongodb:mongodb /edx/var/mongo/.
-  sudo -S service mongod start
-  sudo su edxapp -s /bin/bash
-  source /edx/app/edxapp/edxapp_env
-  cd /edx/app/edxapp/edx-platform
-  echo 'Running Tests'
-  echo $TEST_SUITE 
-  echo $SHARD 
-  ./scripts/travis-tests.sh
-  EOF
+echo 'Fixing Mongo'
+sudo -S rm /edx/var/mongo/mongodb/mongod.lock
+sudo -S mongod -repair --config /etc/mongod.conf
+sudo -S chown -R mongodb:mongodb /edx/var/mongo/.
+sudo -S service mongod start
+sudo su edxapp -s /bin/bash
+source /edx/app/edxapp/edxapp_env
+cd /edx/app/edxapp/edx-platform
+echo 'Running Tests'
+echo $TEST_SUITE 
+echo $SHARD 
+./scripts/travis-tests.sh
+EOF
