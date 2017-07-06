ac0112d313541c04da326b20eac3c8eb76faeeb6
Kabir Khan <kakh@microsoft.com>
2017-07-06 14:58:09 -0700

    Fixing docker test run

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -1,18 +1,36 @@
 #!/usr/bin/env bash
 
+
+# Determine the appropriate github branch to clone using Travis environment variables
+BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
+echo "BRANCH=$BRANCH"
+
 docker exec -i devstack /bin/bash -s <<EOF
+
+# Ensure that MySql is running.  Fixes error "Can't connect to MYSQL server on '127.0.0.1' (111)"
+UP=$(/etc/init.d/mysql status | grep running | grep -v not | wc -l);
+if [[ "$UP" -ne "1" ]]; then echo "Starting MySQL..."; systemctl start mysql.service; fi
+
+# Ensure that Mongo is running
 echo 'Fixing Mongo'
 sudo -S rm /edx/var/mongo/mongodb/mongod.lock
 sudo -S mongod -repair --config /etc/mongod.conf
 sudo -S chown -R mongodb:mongodb /edx/var/mongo/.
 sudo -S service mongod start
+
+# Get the latest code
+cd /edx/app/edxapp/edx-platform
+sudo git pull origin $BRANCH
+
+# Elevate priveleges to edxapp user
 sudo su edxapp -s /bin/bash
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
 echo 'Running Tests'
 echo $TEST_SUITE 
 echo $SHARD 
-cat ./scripts/travis-tests.sh
+export TEST_SUITE=$TEST_SUITE
+export TEST_SUITE=$SHARD
 bash ./scripts/generic-ci-tests.sh
 EOF
 
