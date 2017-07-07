f434ca562bfc98b83b69c93349d3787c0ec18617
Kabir Khan <kakh@microsoft.com>
2017-07-07 14:37:09 -0700

    Fix mongo lang

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -16,6 +16,7 @@ sudo -S rm /edx/var/mongo/mongodb/mongod.lock
 sudo -S mongod -repair --config /etc/mongod.conf
 sudo -S chown -R mongodb:mongodb /edx/var/mongo/.
 sudo -S service mongod start
+export LC_ALL=C
 
 # Get the latest code
 cd /edx/app/edxapp/edx-platform
