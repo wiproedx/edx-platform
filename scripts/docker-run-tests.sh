f265d1f1aec1953c150047e77550dc058654d8cf
Kabir Khan <kakh@microsoft.com>
2017-07-10 12:07:04 -0700

    Fix firefox error with not starting.

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -10,6 +10,7 @@ docker exec -i devstack /bin/bash -s <<EOF
 UP=$(/etc/init.d/mysql status | grep running | grep -v not | wc -l);
 if [[ "$UP" -ne "1" ]]; then echo "Starting MySQL..."; systemctl start mysql.service; fi
 
+
 # Ensure that Mongo is running
 echo 'Fixing Mongo'
 sudo -S rm /edx/var/mongo/mongodb/mongod.lock
@@ -18,19 +19,29 @@ sudo -S chown -R mongodb:mongodb /edx/var/mongo/.
 sudo -S service mongod start
 export LC_ALL=C
 
+
 # Get the latest code
 cd /edx/app/edxapp/edx-platform
 git checkout $BRANCH
 git pull
 
+
 # Elevate priveleges to edxapp user
-sudo su edxapp -s /bin/bash
+echo 'Run as edxapp user'
+sudo su edxapp -s /bin/bash 
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
+
+
 echo 'Running Tests'
 echo $TEST_SUITE 
 echo $SHARD 
+
+# These variables are becoming unset inside docker container
 export TEST_SUITE=$TEST_SUITE
 export SHARD=$SHARD
+
+# Run Firefox with xvfb on DISPLAY :1
+export DISPLAY=:1
 ./scripts/generic-ci-tests.sh
 EOF
