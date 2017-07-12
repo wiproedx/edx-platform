759e6978e839bf274e39c261acabee4b8276fa7d
Kabir Khan <kakh@microsoft.com>
2017-07-12 12:30:21 -0700

    Set python io encoding to fix all unicode test errors

--- scripts/docker-run-tests.sh
+++ scripts/docker-run-tests.sh
@@ -18,7 +18,7 @@ sudo -S mongod -repair --config /etc/mongod.conf
 sudo -S chown -R mongodb:mongodb /edx/var/mongo/.
 sudo -S service mongod start
 export LC_ALL=C
-
+export PYTHONIOENCODING=UTF-8
 
 # Get the latest code
 cd /edx/app/edxapp/edx-platform
