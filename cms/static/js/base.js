8639254e993b21352a938070b0ff041233665853
Kabir Khan <kakh@microsoft.com>
2017-07-20 20:13:35 -0700

    improving quality

--- cms/static/js/base.js
+++ cms/static/js/base.js
@@ -32,10 +32,10 @@ require([
         function selectSite() {
             // Get selected hostname
             var $selectedSite = $(this);
-            var siteName = selectedSite.find('option:selected').val();
+            var siteName = $selectedSite.find('option:selected').val();
 
             // Update button target
-            var buttonElement = selectedSite.parent().parent().find('.view-button');
+            var buttonElement = $selectedSite.parent().parent().find('.view-button');
             var currentUrl = buttonElement.attr('href');
             // todo: replace with a URL builder
             var newUrl = currentUrl.replace(currentUrl.split('/')[2], siteName);
