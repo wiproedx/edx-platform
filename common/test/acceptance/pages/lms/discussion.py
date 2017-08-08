25c182f7bd0e34cb0be23b3ab5ac0aae86a043b1
Kabir Khan <kakh@microsoft.com>
2017-08-08 14:20:40 -0700

    increase timeout more

--- common/test/acceptance/pages/lms/discussion.py
+++ common/test/acceptance/pages/lms/discussion.py
@@ -114,7 +114,7 @@ class DiscussionThreadPage(PageObject, DiscussionPageMixin):
         opened_promise = EmptyPromise(
             lambda: self.is_element_visible(ancestor_selector + " .actions-dropdown"),
             "Secondary action menu opened",
-            timeout=60
+            timeout=300
         )
         opened_promise.fulfill()
         yield
